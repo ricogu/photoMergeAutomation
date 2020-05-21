c17ec308c300efd23be964f0b9c56a22b967a45d
d065300 <yuanxun.gu@sap.com>
2020-05-21 22:41:13 +0200

    improvement

--- photoMerge.sh
+++ photoMerge.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#basePath to set
 basepath="/Volumes/homes/fzelin56/Drive/Moments/Mobile/Zelin iPhone"
+
 
 year=2019
 fromMonth=06
@@ -8,15 +10,16 @@ toMonth=12
 fromDay=13
 toDay=31
 
-
-
 for ((month=$fromMonth;month<=$toMonth;month++))
 do
   mkdir rawVideo
   echo "month: $month"
+
+  #skip the month if video exist
   if [ -f Jessie-$year-$month.mp4 ]
   then
     echo "file Jessie-$year-$month.mp4 exist, go to next month"
+    # reset fromDay for the next month to start from day 1
     fromDay=1
     continue
   fi
@@ -39,8 +42,12 @@ do
     else
       printDay=$day
     fi
+
+    #path based on year, month and day
     path="$basepath/$year-$printMonth-$printDay"
     echo "path: $path"
+
+    #copy from NAS the raw video files
     if [ -d "$path" ]
     then
       count_file=`ls -1 "$path"/*.MOV 2>/dev/null | wc -l`
@@ -59,8 +66,10 @@ do
     fi
   done
 
+  # reset fromDay for the next month to start from day 1
   fromDay=1
 
+  # changing framerate of all mp4 video to 30 fps
   for i in "rawVideo"/*.mp4; do
     ffmpeg -loglevel panic -n -i "$i" -vf "setpts=1.25*PTS" -r 30 "${i%.*}_30fps.mp4"
     echo "Converted $i into ${i%.*}_30fps.mp4"
@@ -73,12 +82,16 @@ do
   done
 
 
-
+  # find all converted videos with 30 fps
   find rawVideo/*_30fps.mp4 | sed 's:\ :\\\ :g'| sed 's/^/file /' >> index.txt
 
+  # merge all of them
   echo "Merging all mp4 videos"
   ffmpeg -loglevel panic -safe 0 -f concat -i index.txt -n -c copy Jessie-$year-$month.mp4
+
+  #cleanup
   rm -rf rawVideo
+  rm index.txt
   
 done
 
